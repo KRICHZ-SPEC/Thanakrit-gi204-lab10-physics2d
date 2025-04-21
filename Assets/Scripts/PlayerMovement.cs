94b7d7b861d1259d6139c10ad18d99994f53f9ea
KRICHZ-SPEC <thanakrit.chang@bumail.net>
2025-04-22 01:45:45 +0700

    script playermovement & jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d; //private variable
+    //walk left-right
+    private float move; // store input from player
+    [SerializeField] private float speed;
+    //jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isjumping;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        //walk left-right
+       move = Input.GetAxis("Horizontal");
+       rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+       //jump
+       if (Input.GetButtonDown("Jump") && !isjumping)
+       {
+           rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+           Debug.Log("Jump!"); // for debugging
+       }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = true;
+        }
+    }
+}
